1483ec13d5537a3ed0a2b7bb4ff15230d7e266a3
Aleena <aleenabiju09@gmail.com>
2022-12-29 02:27:19 +0530

    edited

--- app.js
+++ app.js
@@ -14,17 +14,22 @@ app.get("/",(req,res)=>{
     //console.log(req)
 })
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+//my api key is stored in an environment variable of my system 
+const apiKey = JSON.parse(process.env['WEATHER_API_KEY']);
 app.post("/",(req,res)=>{
     console.log("Post recieved")
     console.log(req.body.city)
-    const apiKey = "6a4ecd673b8949f1abd190407222712"
+    console.log(apiKey)
     const loc = req.body.city
     const url = "https://api.weatherapi.com/v1/current.json?key="+apiKey+"&q="+loc
     https.get(url,(response)=>{
         console.log(response.statusCode)
         response.on("data",(data)=>{
             const weatherData = JSON.parse(data)
-            const region = weatherData.location.region
+            console.log(weatherData)
             const country = weatherData.location.country
 
             const localtime = weatherData.location.localtime
@@ -33,16 +38,29 @@ app.post("/",(req,res)=>{
             const splitDate = dateGiven.split("-")
             const date = splitDate[2]+"-"+splitDate[1]+"-"+splitDate[0]
 
-            const temp = weatherData.current.temp_c
+            // const temp = weatherData.current.temp_c
+            // const feelsLike = weatherData.current.feelslike_c
+            const temp = weatherData.current.temp_f
+            const feelsLike = weatherData.current.feelslike_f
+
+            // const temp_unit = "&#8451;" //deg C
+            const temp_unit = "&#8457;" //deg F
+            
             const desc = weatherData.current.condition.text
-            const iconURL = weatherData.current.condition.icon
-            const feelsLike = weatherData.current.feelslike_c
+            const iconURL = weatherData.current.condition.icon 
             const humidity = weatherData.current.humidity
             const cloud = weatherData.current.cloud
             const uv = weatherData.current.uv
-            const vis = weatherData.current.vis_km
-            const wind_speed = weatherData.current.wind_kph
             const wind_dir = weatherData.current.wind_dir
+
+            // const vis = weatherData.current.vis_km
+            // const wind_speed = weatherData.current.wind_kph
+            const vis = weatherData.current.vis_miles
+            const wind_speed = weatherData.current.wind_mph
+
+            // const dist_unit = "km"
+            const dist_unit = "miles"
+            
 
             //sending all this data to the webpage
             const imageURL = "images/card_bg.jpg"
@@ -50,13 +68,13 @@ app.post("/",(req,res)=>{
             res.write("<body style='font-family:Helvetica;background-image:url("+imageURL+");background-size:cover;'>")
             res.write("<div style='margin:2.5% 25% 0% 25%;width:50%;display:grid;place-items:center;align-items:center;background-color:rgba(209, 237, 255, 0.65);border-radius:50px;z-index:10;box-shadow:25px 25px 75px black;'><h1 style='margin:10%;'>Weather Today</h1>")
             res.write("<div style='margin-bottom:10%;width:100%;display:flex;place-contents:center;align-items:center;'>")
-            res.write("<div style='width:25%;height:290px;padding-left:10%;padding-right:13.5%;border-right:2px solid darkslategrey;'><h2>"+temp+" &#8451;</h2><h3>"+desc+"</h3><img src="+iconURL+" alt='weather icon' width=80px height=80px><p>"+time+", "+date+"</p><p>"+loc+", "+country+"</p></div>")
-            res.write("<div style='width:auto;height:290px;padding-left:10%;padding-right:10%;border-left:2px solid darkslategrey;'><p>Feels Like: "+feelsLike+" &#8451;</p>")
+            res.write("<div style='width:25%;height:290px;padding-left:10%;padding-right:13.5%;border-right:2px solid darkslategrey;'><h2>"+temp+" "+temp_unit+"</h2><h3>"+desc+"</h3><img src="+iconURL+" alt='weather icon' width=80px height=80px><p>"+time+", "+date+"</p><p>"+loc+", "+country+"</p></div>")
+            res.write("<div style='width:auto;height:290px;padding-left:10%;padding-right:10%;border-left:2px solid darkslategrey;'><p>Feels Like: "+feelsLike+" "+temp_unit+"</p>")
             res.write("<p>Humidity: "+humidity+" %</p>")
             res.write("<p>UV Index: "+uv+"</p>")
             res.write("<p>Cloud Cover: "+cloud+" %</p>")
-            res.write("<p>Visibility: "+vis+" km</p>")
-            res.write("<p>Wind Speed: "+wind_speed+" km/h</p>")
+            res.write("<p>Visibility: "+vis+" "+dist_unit+"</p>")
+            res.write("<p>Wind Speed: "+wind_speed+" "+dist_unit+"/h</p>")
             res.write("<p>Wind Direction: "+wind_dir+"</p></div></div></div>")
 
             res.write("</body></html>")
